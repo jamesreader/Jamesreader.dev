a1e4e6251b1bd6e84a97b9e18bf277876e9d0963
root <root@iris-openclaw.tail791026.ts.net>
2026-03-26 11:43:10 -0600

    Add version indicator: meta tag + body data-version (2026.03.26.1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="site-version" content="2026.03.26.1" />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -48,7 +49,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="bg-cream dark:bg-dark-bg text-charcoal dark:text-dark-text antialiased">
+      <body data-version="2026.03.26.1" className="bg-cream dark:bg-dark-bg text-charcoal dark:text-dark-text antialiased">
         <AgentProvider>
           <LayoutShell>
             {children}
